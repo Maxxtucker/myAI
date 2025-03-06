716edddf2e91b9a9768c0dc24893827b10207589
Tucker-Xiaoxiao-wego <tucker@wego.com>
2025-03-06 17:28:44 -0500

    add ai identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Xiaoxiao`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `My name is Xiaoxiao, penultimate-year student at NUS`;
 
 export const AI_NAME: string = `Tucker`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Friendly`;
+export const AI_ROLE: string = `A digital extension of myself that helps display my CV, skills, interests, and learning progress.`;
