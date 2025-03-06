55473a9b783b2d90e5804158aea034a9327c8955
Tucker-Xiaoxiao-wego <tucker@wego.com>
2025-03-06 17:15:38 -0500

    change ui

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[Customised AI by Xiaoxiao]`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR]`;
 export const PAGE_TITLE: string = `[Xiaoxiao's AI assistant`;
-export const PAGE_DESCRIPTION: string = `Chat with ${'Tucker'}, ${'Xiaoxiao'}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${'AI_NAME'}, ${'OWNER_NAME'}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
